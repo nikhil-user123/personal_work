033e621126293d053218b5a087b32e2d0304b99f
nikhil-user123 <nikhilprajapat@gmail.com>
2022-08-20 06:30:53 +0530

    commint

--- Component/navbar.js
+++ Component/navbar.js
@@ -15,7 +15,18 @@ import styles from '../styles/index.module.css';
                     <Link href="/" className={styles.text} >HelpCenter</Link>
                 </li>    
                 </ul>
+                <input type="checkbox" name="click" id="click">
+
+                <label for="" className={styles.search}>
+                <i className={fa fa-search} aria-hidden="true"></i>    
+                </label>
+
+                
+
+                <div className={styles.button}>
+                <button  className={styles.button_Sub}>Submit a Request</button>
                 <button  className={styles.button_sign}>Sign in</button>
+                </div>
            </nav>
     </> 
  ); 
